bc52a8e0f1e74f721eb8c8332edd91b7a86c954a
Brad Sherman <bsherman1096@gmail.com>
2020-06-01 22:05:02 -0500

    Scraping game metadata

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,34 +1,61 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Lib
-    ( someFunc
-    ) where
+  ( scrapeGameStatsFromUrl
+  )
+where
 
 import           Control.Applicative
+import           System.Environment
 import           Text.HTML.Scalpel
 
 type Team = String
 
 data TeamScore = TeamScore Team Int
+  deriving (Show, Eq)
+
+-- type Date = String
+-- type StartTime = String
+-- this is just a list of tags for now, an extra function will be required
+-- to parse certain things
+data GameMeta = GameMeta [String]
     deriving (Show, Eq)
 
-data GameScore = GameScore TeamScore TeamScore
+data GameStats = GameStats TeamScore TeamScore GameMeta
     deriving (Show, Eq)
 
-gameScore :: Scraper String [TeamScore]
-gameScore = chroots ("div" @: [hasClass "scorebox"] // "div") teamScore
+gameScore :: Scraper String GameStats
+gameScore = chroot ("div" @: ["id" @= "content"]) teamScores
+
+teamScores :: Scraper String GameStats
+teamScores = do
+  scores    <- chroots ("div" @: [hasClass "scorebox"] // "div") teamScore
+  game_meta <- chroot ("div" @: [hasClass "scorebox_meta"]) gameMeta
+  return $ GameStats (head scores) (last scores) game_meta
 
 teamScore :: Scraper String TeamScore
 teamScore = do
-  name <- text $ "strong" // "a" @: ["itemprop" @= "name"]
+  name  <- text $ "strong" // "a" @: ["itemprop" @= "name"]
   score <- text $ "div" @: [hasClass "scores"] // "div" @: [hasClass "score"]
   return $ TeamScore name (read score)
 
-type Author = String
-data Comment = TextComment Author String
-    | ImageComment Author URL
-    deriving (Show, Eq)
+gameMeta :: Scraper String GameMeta
+gameMeta = do
+  meta <- chroots ("div" // "div") gameTags
+  return $ GameMeta meta
 
-someFunc :: IO ()
-someFunc = do
-  output <- scrapeURL "https://www.baseball-reference.com/boxes/MIA/MIA201903280.shtml" gameScore
+gameTags :: Scraper String String
+gameTags = do
+  tag <- text $ "div"
+  return $ tag
+
+getStatsFromUrl :: URL -> IO ()
+getStatsFromUrl url = do
+  output <- scrapeURL url gameScore
   print output
+
+scrapeGameStatsFromUrl :: IO ()
+scrapeGameStatsFromUrl = getArgs >>= handleArgs
+
+handleArgs :: [String] -> IO ()
+handleArgs [url] = getStatsFromUrl url
+handleArgs _     = putStrLn "usage: scout <URL>"
